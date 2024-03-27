30c9e36c2d41eadfcaa08c1f473ebc587c3b02c9
jae9380 <ljy5314@gmail.com>
2024-03-27 23:44:40 +0900

    hotfix: Modifying FCMConfig

--- src/main/java/com/ll/gooHaeYu/global/config/FCMConfig.java
+++ src/main/java/com/ll/gooHaeYu/global/config/FCMConfig.java
@@ -6,6 +6,8 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -19,21 +21,19 @@ public class FCMConfig {
 
     static private final String FIREBASE_ADMIN_SDK_PATH = "firebase/goohaeyou-firebase-adminsdk.json";
 
+    private static final Logger logger = LoggerFactory.getLogger(FCMConfig.class);
+
     @PostConstruct
     public void initialize() {
-        ClassPathResource resource = new ClassPathResource(FIREBASE_ADMIN_SDK_PATH);
-
-        try (InputStream is = resource.getInputStream()) {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(is))
-                    .build();
-
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(FIREBASE_ADMIN_SDK_PATH).getInputStream())).build();
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
-                log.info("FirebaseApp initialization complete");
+                logger.info("Firebase application has been initialized");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
         }
     }
 }
